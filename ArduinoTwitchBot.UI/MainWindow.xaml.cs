412e4a7e5fe62d52fa3fe459dd2ebc8c722c0e7e
Rafał Klinowski <klinowskirafal@gmail.com>
2021-02-21 14:19:30 +0100

    Initial commit

--- ArduinoTwitchBot.UI/MainWindow.xaml.cs
+++ ArduinoTwitchBot.UI/MainWindow.xaml.cs
@@ -1,28 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO.Ports;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ArduinoTwitchBot.UI
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			PortSelectionBox.ItemsSource = SerialPort.GetPortNames();
+			PortSelectionBox.SelectedIndex = 0;
+		}
+
+		private void HelpButton_Click(object sender, RoutedEventArgs e)
+		{
+			Process.Start("https://dev.twitch.tv/docs/authentication#registration");
+		}
+
+		private void ConnectionTestButton_Click(object sender, RoutedEventArgs e)
+		{
+			var text = ConnectionTestBox.Text;
+
+			if (text == "" || string.IsNullOrWhiteSpace(text))
+			{
+				MessageBox.Show("The entered value must be a non-empty string.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				return;
+			}
+
+			var port = new SerialPort(PortSelectionBox.SelectedValue.ToString())
+			{
+				BaudRate = 9600,
+				ReadTimeout = 2000,
+				WriteTimeout = 2000
+			};
+
+			try
+			{
+				port.Write(text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
+
+		private void ShowHideApiKey_PreviewMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			if (ShowHideApiKey.Kind == MaterialDesignThemes.Wpf.PackIconKind.Eye)
+			{
+				ShowHideApiKey.Kind = MaterialDesignThemes.Wpf.PackIconKind.EyeOff;
+
+				var enteredApiKey = ApiKeyBox.Password;
+
+				VisibleApiKeyBox.Visibility = Visibility.Visible;
+				VisibleApiKeyBox.Text = enteredApiKey;
+				ApiKeyBox.Visibility = Visibility.Collapsed;
+			}
+			else
+			{
+				ShowHideApiKey.Kind = MaterialDesignThemes.Wpf.PackIconKind.Eye;
+
+				var enteredPassword = VisibleApiKeyBox.Text;
+
+				ApiKeyBox.Visibility = Visibility.Visible;
+				ApiKeyBox.Password = enteredPassword;
+				VisibleApiKeyBox.Visibility = Visibility.Collapsed;
+			}
 		}
 	}
 }
